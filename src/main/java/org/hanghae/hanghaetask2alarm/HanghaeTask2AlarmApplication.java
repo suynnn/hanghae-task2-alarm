5cb78f343b8dbce774fae67b5bf3e1f683a18d9b
suynnn <suy00065@gmail.com>
2024-10-14 09:53:32 +0900

    Init: 프로젝트 초기설정

---
+++ src/main/java/org/hanghae/hanghaetask2alarm/HanghaeTask2AlarmApplication.java
@@ -0,0 +1,13 @@
+package org.hanghae.hanghaetask2alarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeTask2AlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeTask2AlarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hanghae/hanghaetask2alarm/HanghaeTask2AlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hanghae.hanghaetask2alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeTask2AlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
